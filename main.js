cb835039b95fd8e46ed1c5862a4a1c970a59466a
Ing Erick Cruz <drerick.chuzra@gmail.com>
2025-04-20 18:17:41 -0400

    I added a new endpoint to obtain music

--- main.js
+++ main.js
@@ -5,13 +5,18 @@ const fs = require("fs");
 const app = express();
 const upload = multer({ dest: "uploads/" }); // Carpeta donde se guardarán los archivos
 const UPLOADS_FOLDER = "uploads";
+const UPLOADS_IMAGE = "uploads/images";
+const UPLOADS_VIDEO = "uploads/videos";
+const UPLOADS_MUSIC = "uploads/music";
 const cors = require("cors");
-
+const { url } = require("inspector");
 
 app.use("/uploads", express.static("uploads"));
-app.use(cors({
-  origin:"http://localhost:4200"
-}))
+app.use(
+  cors({
+    origin: ["http://192.168.0.8:4200", "http://localhost:4200"],
+  })
+);
 // Página HTML para subir archivos
 function saveFiles(file) {
   const newPath = `./uploads/${file.originalname}`;
@@ -28,7 +33,7 @@ app.get("/", (req, res) => {
   `);
 });
 //here look all or I think so
-app.get("/files", (req, res) => {
+app.get("/images", (req, res) => {
   /* fs.readdir(UPLOADS_FOLDER, (err, files) => {
     if (err) {
       return res.status(500).send("Error al leer la carpeta.");
@@ -40,16 +45,29 @@ app.get("/files", (req, res) => {
 
     res.send(`<h2>Archivos disponibles</h2><ul>${fileList}</ul>`); */
 
-    fs.readdir(UPLOADS_FOLDER, (err, files) => {
+  fs.readdir(UPLOADS_IMAGE, (err, files) => {
     if (err) {
       return res.status(500).json({ error: "Error al leer la carpeta." });
     }
 
     const fileLinks = files.map((file) => ({
       name: file,
-      url: `/uploads/${encodeURIComponent(file)}`,
+      url: `/uploads/images/${encodeURIComponent(file)}`,
     }));
-    console.log(fileLinks)
+    console.log(fileLinks);
+    res.json(fileLinks);
+  });
+});
+app.get("/music", (req, res) => {
+  fs.readdir(UPLOADS_MUSIC, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: "Error al leer la carpeta." });
+    }
+    const fileLinks = files.map((file) => ({
+      name: file,
+      url: `/uploads/music/${encodeURIComponent(file)}`,
+    }));
+    console.log(fileLinks);
     res.json(fileLinks);
   });
 });
@@ -59,7 +77,6 @@ app.post("/upload", upload.single("file"), (req, res) => {
   saveFiles(req.file);
   res.send(`Archivo subido: ${req.file.originalname}`);
 });
-
 
 // Inicia el servidor
 const PORT = 8000;
